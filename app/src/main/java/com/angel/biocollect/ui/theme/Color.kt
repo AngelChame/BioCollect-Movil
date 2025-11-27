1dd7e69d4ba8a93b73243f2090f67acbbcdb0da4
AngelChame <chapperzz3@gmail.com>
2025-11-27 15:33:55 -0600

    theme update

--- app/src/main/java/com/angel/biocollect/ui/theme/Color.kt
+++ app/src/main/java/com/angel/biocollect/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.angel.biocollect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFF7CB342)
+val GreenGrey80 = Color(0xFF558B2F)
+val LightGreen80 = Color(0xFF8BC34A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green40 = Color(0xFF689F38)
+val GreenGrey40 = Color(0xFF33691E)
+val LightGreen40 = Color(0xFF9CCC65)260)
